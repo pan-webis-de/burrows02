2b934a7ce976defe2e575cde1a883485bd3001fc
namsap <ropass@posteo.de>
2015-09-19 00:40:07 +0200

    Add a method for testing an idea for open-class problems

--- testcases.py
+++ testcases.py
@@ -171,6 +171,92 @@ def pan12(problem, problem_training, n_authors, n_training, n_testcases,
                     print(str(deltas[(author,considered_words)]), end=",")
                 print("\n", end="")       
     print("\n\n")
+    
+
+def pan12open(problem, problem_training, n_authors, n_training, n_testcases, 
+          considered_words_list, real_words, training_path, test_path, thresholds):
+    """
+    Run the algorithm with PAN12 data. 
+    This should be used for open-class problems.
+    
+    Keyword arguments:
+    problem -- The problem of PAN12 ('A','B' etc.)
+    problem_training -- Using the training data of this problem.
+    n_authors -- The number of authors of this case.
+    n_training -- The number of training files per author.
+    n_testcases -- The number of test cases.
+    considered_words -- How many of the most common words should be considered?
+    real_words -- Only alphabetic words?
+    training_path -- The path to the PAN12 training folder
+    test_path -- The path to the PAN12 test folder
+    thresholds -- A list of thresholds.
+    """    
+    print("Problem: PAN12 " + problem)
+    print("real_words: " + str(real_words))    
+    
+    database = Database(0, real_words)
+    
+    # Load the training data
+    for author_letter in string.ascii_uppercase[0:n_authors:]:
+        author = Author("Author " + author_letter)
+        for testcase_number in range(1,n_training+1):
+             # "12AtrainA1.txt"
+            testcase = Text(raw_from_file(
+                training_path + "12" + problem_training + "train" + author_letter 
+                              + str(testcase_number) + ".txt"),
+                "Training " + author_letter + " " + str(testcase_number))
+            author.add_text(testcase)
+        database.add_author(author)
+    database.process()
+    
+    for author in database.authors:
+        author.calc_cmsz(database)
+        
+    # Next, do the testcases
+    for testcase_number in range(1,n_testcases+1):   
+        testcase = Text(raw_from_file(
+            test_path + "12" + problem + "test"
+                      +  str(testcase_number).zfill(2) + ".txt"),
+            "Test case " + problem + " " + str(testcase_number))
+            
+        print("\nDeltas for Test case " + str(testcase_number) + ":")
+        
+        for considered_words in considered_words_list:
+            database.considered_words = considered_words
+            print("\nconsidered_words: " + str(considered_words))
+
+            # We have to process the database again since we restrict to n words
+            database.process()
+        
+            testcase.calc_zscores(database)
+
+            # now calculate the deltas
+            deltas = {}
+            for author in database.authors:
+                deltas[author] = testcase.calc_delta(database,author)
+            
+            for author in database.authors:
+                print(author.name + ": " + str(deltas[author]))
+                
+            # which is the minimal?
+            author_min = min(deltas, key=deltas.get)
+            
+            deltas_normal = {}
+            for author in database.authors:
+                deltas_normal[author] = deltas[author] / deltas[author_min]
+                
+            for threshold in thresholds:
+                print("Threshold " + str(threshold), end=": ")
+                
+                # check if there are any elements below the threshold
+                counterexamples = {k: v for k, v in deltas_normal.items() 
+                                                 if v > 1 and v < threshold}
+                if counterexamples:
+                    print("None")
+                else:
+                    print(author_min.name)
+       
+    print("\n\n")
   
   
 def main():
@@ -208,6 +294,26 @@ def main():
               n_authors=14, n_training=2, n_testcases=16,
               considered_words_list=testcases, real_words=True,
               training_path=training_path,test_path=test_path)
+    
+    thresholds = [1.01,1.025,1.05,1.10,1.20]          
+    
+    pan12open(problem="B", problem_training="A",
+              n_authors=3, n_training=2, n_testcases=10,
+              considered_words_list=testcases, real_words=True,
+              training_path=training_path,test_path=test_path,
+              thresholds=thresholds)
+              
+    pan12open(problem="D", problem_training="C",
+              n_authors=8, n_training=2, n_testcases=17,
+              considered_words_list=testcases, real_words=True,
+              training_path=training_path,test_path=test_path, 
+              thresholds=thresholds)
+              
+    pan12open(problem="J", problem_training="I",
+              n_authors=14, n_training=2, n_testcases=16,
+              considered_words_list=testcases, real_words=True,
+              training_path=training_path,test_path=test_path,
+              thresholds=thresholds)
 
 if __name__ == "__main__":
     # execute only if run as a script
